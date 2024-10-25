0955f5ec9b8475b68fbbe62afa70b81564c15b05
Daniel <daniel.perez19@udea.edu.co>
2024-10-24 23:37:39 -0500

    Ahora el calendario si puede cambiar entre meses, se mejora el aspecto estilistico

--- Proyecto_Final/Codigo/calendar.js
+++ Proyecto_Final/Codigo/calendar.js
@@ -9,29 +9,37 @@ let dates = document.getElementById('dates');
 let month = document.getElementById('month');
 let year = document.getElementById('year');
 
+let prevMonthDOM = document.getElementById('prev-month');
+let nextMonthDOM = document.getElementById('next-month');
+
 month.textContent = monthNames[monthNumber];
 year.textContent = currentYear.toString();
 
-const writeMonth = () => {
+prevMonthDOM.addEventListener('click', () => lastMonth());
+nextMonthDOM.addEventListener('click', () => nextMonth());
+
+const writeMonth = (month) => {
     for(let i = startDay(); i > 0; i--) {
-        dates.innerHTML += `<div class="calendar__date calendar__last-days">
-            ${getTotalDays(monthNumber) - (i - 1)}
+        dates.innerHTML += `<div class="calendar__date calendar__item calendar__last-days">
+            ${getTotalDays(monthNumber - 1) - (i - 1)}
         </div>`;
     }
 
-    for(let i = 1; i <= getTotalDays(monthNumber); i++) {
+    for(let i = 1; i <= getTotalDays(month); i++) {
         if(i === currentDay) {
-            dates.innerHTML += `<div class="calendar__date calendar__today">${i}</div>`;
+            dates.innerHTML += `<div class="calendar__date calendar__item calendar__today">${i}</div>`;
         } else {
-            dates.innerHTML += `<div class="calendar__date">${i}</div>`;
+            dates.innerHTML += `<div class="calendar__date calendar__item">${i}</div>`;
         }
     }
 }
 
 const getTotalDays = (month) => {
-    if(month === 0 || month === 2 || month === 4 || month === 6 || month === 7 || month === 9 || month === 11) {
+    if(month === -1) month = 11;
+
+    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
         return 31;
-    } else if (month === 3 || month === 5 || month === 8 || month === 10) {
+    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
         return 30;
     } else {
         return 28;
@@ -43,4 +51,34 @@ const startDay = () => {
     return ((start.getDay() - 1) === -1) ? 6 : start.getDay() - 1;
 }
 
-writeMonth();
+const lastMonth = () => {
+    if(monthNumber !== 0) {
+        monthNumber--;
+    } else {
+        monthNumber = 11;
+        currentYear--;
+    }
+
+    setNewDate();
+}
+
+const nextMonth = () => {
+    if(monthNumber !== 11) {
+        monthNumber++;
+    } else {
+        monthNumber = 0;
+        currentYear++;
+    }
+
+    setNewDate();
+}
+
+const setNewDate = () => {
+    currentDate.setFullYear(currentYear, monthNumber, currentDay);
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}
+
+writeMonth(monthNumber);
