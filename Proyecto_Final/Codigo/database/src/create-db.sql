d9ff0c4bd227db8fb274c98d8c4ed223b89ea6f5
Daniel <daniel.perez19@udea.edu.co>
2024-11-14 23:23:17 -0500

    Corecciones en la BD

--- Proyecto_Final/Codigo/database/src/create-db.sql
+++ Proyecto_Final/Codigo/database/src/create-db.sql
@@ -1,20 +1,20 @@
-BEGIN;
+BEGIN TRANSACTION;
 
-CREATE TABLE User (
-    id_user INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Users (
+    id_user INT PRIMARY KEY,
     names VARCHAR(100) NOT NULL,
     passwords VARCHAR(255) NOT NULL,  -- Asegúrate de almacenar el hash, no el texto plano
     is_admin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE Events (
-    id_event INT PRIMARY KEY AUTO_INCREMENT,
+    id_event INT PRIMARY KEY,
     event_name VARCHAR(100) NOT NULL,
     locations VARCHAR(100),
     date_start DATE,
     date_finish DATE,
     id_user INT,
-    FOREIGN KEY (id_user) REFERENCES User(id_user)
+    FOREIGN KEY (id_user) REFERENCES Users(id_user)
 );
 
 -- Tabla intermedia para la relación muchos a muchos entre usuario y evento
@@ -22,7 +22,7 @@ CREATE TABLE Participation (
     id_user INT,
     id_event INT,
     PRIMARY KEY (id_user, id_event),
-    FOREIGN KEY (id_user) REFERENCES User(id_user),
+    FOREIGN KEY (id_user) REFERENCES Users(id_user),
     FOREIGN KEY (id_event) REFERENCES Events(id_event)
 );
 
