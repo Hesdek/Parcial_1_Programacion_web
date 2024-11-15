ea0ba4436a2228c371dccfbc711fa406d3af367a
Daniel <daniel.perez19@udea.edu.co>
2024-11-15 17:59:42 -0500

    Inicio parcial 3

--- Proyecto_Final/Codigo/database/src/insert-db.sql
+++ Proyecto_Final/Codigo/database/src/insert-db.sql
@@ -1,8 +1,8 @@
-BEGIN;
+BEGIN TRANSACTION;
 
 
 INSERT INTO
-    User (names, passwords, is_admin)
+    Users (names, passwords, is_admin)
 VALUES
     ('Juan Perez', 'password123', FALSE),
     ('Bob Martinez', 'hashed_password2', TRUE),
@@ -16,14 +16,14 @@ VALUES
     ('Feria de las Flores', 'Medellín', '2024-08-03', '2024-08-12', 2), --bob (id_user=2) es admin
     ('Desfile de Silleteros', 'Avenida del Río, Medellín', '2024-08-11', '2024-08-11', 2),
     ('Concierto de Apertura', 'Plaza Mayor, Medellín', '2024-08-02', '2024-08-02', 2),
-    ('Festival Nacional de la Trova', 'Teatro Metropolitano', '2024-08-10', '2024-08-10', 4),
+    ('Festival Nacional de la Trova', 'Teatro Metropolitano', '2024-08-10', '2024-08-10', 4);
 
 
 INSERT INTO 
     Participation (id_user, id_event) 
 VALUES 
-    (1, 1); -- juan Feria de las Flores
-    (1, 3); -- juan Concierto de Apertura
+    (1, 1), -- juan Feria de las Flores
+    (1, 3), -- juan Concierto de Apertura
     (3, 4); -- daniela Festival Nacional de la Trova
 
 COMMIT;
